2fbd9f058c8e6141c8f985db766195722adba325
Onur <onurnis69@gmail.com>
2020-12-12 13:52:19 +0300

    nnnnnnnnnnnnnnn

--- src/test/java/runners/FailedRunner.java
+++ src/test/java/runners/FailedRunner.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+
         plugin = {"pretty"},
         features = {"@target/failed.txt"},
         glue = {"stepdefinitions"}
